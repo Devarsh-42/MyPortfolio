09641427d1f500beeca4a85db3c09b25c7850a44
Devarsh-42 <devarshmehta2005@gmail.com>
2025-07-06 21:22:14 +0530

    Menu Icon fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img 
           className={styles.menuBtn} 
-          src={getImageUrl("navbar/menuIcon.png")} 
+          src={getImageUrl("nav/menuIcon.png")} 
           alt="menu-button" 
           onClick={() => setMenuOpen(!menuOpen)}
         />
